80c0635f390e28abd96619cf8580e4e8d0a08935
crawler364 <crawler364@gmail.com>
2020-12-15 16:50:53 +0300

    1723.31

--- lib/main/tools.php
+++ lib/main/tools.php
@@ -4,7 +4,6 @@
 namespace WC\Main;
 
 
-use Bitrix\Highloadblock\HighloadBlockTable;
 use Bitrix\Main\Web\Json;
 
 class Tools
@@ -41,55 +40,6 @@ class Tools
             $users[] = $user;
         }
         return $users;
-    }
-
-    public static function getUserManagers($userID)
-    {
-        if (!\ALG\User::IsMainUser($userID)) {
-            $userID = \ALG\User::getMainUserId($userID);
-        }
-        if (!$managersGroupsID = self::getUserField('UF_MANAGERS_GROUPS', $userID)) {
-            return null;
-        }
-
-        $arFilter = ['IBLOCK_ID' => \WC\IBlock\Tools::getIBlockIDByCode('MANAGERS_GROUPS'), 'ID' => $managersGroupsID];
-        $arSelect = ['IBLOCK_ID', 'ID', 'PROPERTY_MANAGER'];
-        $res = \CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
-        while ($ar = $res->GetNext()) {
-            $entity = HighloadBlockTable::compileEntity('Polzovateli');
-            $entityDataClass = $entity->getDataClass();
-            $res2 = $entityDataClass::getList([
-                'select' => ['UF_NAME', 'UF_MANAGER_EMAIL'],
-                'filter' => ['UF_XML_ID' => $ar['PROPERTY_MANAGER_VALUE']],
-            ]);
-            if ($ar2 = $res2->Fetch()) {
-                if (check_email($ar2['UF_MANAGER_EMAIL'])) {
-                    $email = $ar2['UF_MANAGER_EMAIL'];
-                }
-                $managers[] = ['NAME' => $ar2['UF_NAME'], 'EMAIL' => $email];
-            }
-        }
-        return $managers;
-    }
-
-    public static function getStoreOperators($storeID)
-    {
-        $obStore = \CCatalogStore::GetList([], ['ID' => $storeID], false, false, ['UF_STORE_MANAGER']);
-        if ($arStore = $obStore->fetch()) {
-            $entity = HighloadBlockTable::compileEntity('Polzovateli');
-            $entityDataClass = $entity->getDataClass();
-            $res = $entityDataClass::getList([
-                'select' => ['UF_MANAGER_EMAIL'],
-                'filter' => ['ID' => $arStore['UF_STORE_MANAGER']],
-            ]);
-            if ($ar = $res->Fetch()) {
-                if (check_email($ar['UF_MANAGER_EMAIL'])) {
-                    $email = $ar['UF_MANAGER_EMAIL'];
-                }
-                $operators[] = ['NAME' => $ar['UF_NAME'], 'EMAIL' => $email];
-            }
-        }
-        return $operators;
     }
 
     public static function getEmailsFromArray($array)
--- lib/sale/tools.php
+++ lib/sale/tools.php
@@ -11,4 +11,17 @@ class Tools
         $registry = \Bitrix\Sale\Registry::getInstance(\Bitrix\Sale\Registry::REGISTRY_TYPE_ORDER);
         $registry->set(constant("\Bitrix\Sale\Registry::$entity"), $class);
     }
+
+    public static function getOrderPropertyItemByCode(\Bitrix\Sale\Order $order, $code)
+    {
+        $pc = $order->getPropertyCollection();
+
+        foreach ($pc as $property) {
+            if ($property->getField('CODE') === $code) {
+                return $property;
+            }
+        }
+
+        return null;
+    }
 }
