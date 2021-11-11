f1e1aedb7e1d309341bd72736f9ab90c70e59322
crawler364 <crawler364@gmail.com>
2021-11-11 21:11:18 +0300

    0.3.11

--- lib/helpers/sale.php
+++ lib/helpers/sale.php
@@ -14,17 +14,4 @@ class Sale
         $registry = Registry::getInstance(Registry::REGISTRY_TYPE_ORDER);
         $registry->set(constant("\Bitrix\Sale\Registry::$entity"), $class);
     }
-
-    public static function getOrderPropertyItemByCode(Order $order, $code)
-    {
-        $pc = $order->getPropertyCollection();
-
-        foreach ($pc as $property) {
-            if ($property->getField('CODE') === $code) {
-                return $property;
-            }
-        }
-
-        return null;
-    }
 }
